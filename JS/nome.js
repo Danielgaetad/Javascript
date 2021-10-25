e4596be1848098291be6cbcbbc3281b065b98f04
Daniela Gaeta Dias <79600536+Danielgaetad@users.noreply.github.com>
2021-10-25 16:56:47 -0300

    Add files via upload

---
+++ JS/nome.js
@@ -0,0 +1,10 @@
+var nome = prompt("Digite seu nome");
+//console.log(nome);
+
+alert("Bem vindo " + nome)
+
+var num1 = parseInt(prompt("Primeiro número"));
+var num2 = parseInt(prompt("Segundo número"));
+var soma = num1 + num2;
+
+alert("A soma é " + soma);
---
+++ JS/soma.js
@@ -0,0 +1,5 @@
+var num1 = prompt("Primeiro número");
+var num2 = prompt("Segundo número");
+var soma = parseInt(num1) + parseInt(num2);
+
+document.getElementById('resultado').innerHTML = soma;
